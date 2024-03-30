e4acf297180773543d5dcbf32df359939f4945de
Pavel <pavelkurek@seznam.cz>
2024-03-30 15:17:38 +0100

    DONE

--- client/src/services/pdfCreator.tsx
+++ client/src/services/pdfCreator.tsx
@@ -1,43 +1,68 @@
 import React from 'react';
-import ReactPDF, { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import ReactPDF, {Page, Text, View, Document, StyleSheet, Image} from '@react-pdf/renderer';
 import Roboto from '../assets/Roboto-Regular.ttf'
 import Font = ReactPDF.Font;
-
+import img from '../assets/diplomy_2.jpg'
+import Wednesday from '../assets/Wednesday.ttf'
 
 Font.register({
-    family: 'Roboto',
-    src: Roboto
+    family: 'Wednesday',
+    src: Wednesday
 })
+
 const styles = StyleSheet.create({
     page: {
         flexDirection: 'column',
-        backgroundColor: '#E4E4E4',
-        justifyContent: 'center',
         alignItems: 'center',
-        fontFamily: 'Roboto'
+        fontFamily: 'Wednesday',
+
     },
     section: {
         margin: 10,
+        marginTop: 250,
         padding: 10,
         flexGrow: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        position: "absolute",
+        zIndex: 0,
     },
+    row: {
+        padding: 8,
+        fontSize: 40,
+    },
+    footer: {
+        marginTop: 130,
+        padding: 8,
+        fontSize: 40,
+    },
+    heading:{
+        padding: 8,
+        fontSize: 60,
+        marginBottom: 50,
+    },
+    image: {
+        zIndex: 0,
+        position: "absolute",
+        minWidth: 100,
+        minHeight: 100,
+    }
 });
 
 const MyDocument = (props: { objects: { [key: string]: string }[] }) => (
     <Document>
-
-            {props.objects.map((obj, index) => (
-                <Page key={index}  size="A4" style={styles.page}>
+        {props.objects.map((obj, index) => (
+            <Page key={index} size="A4" style={styles.page}>
+                <Image src={img}/>
                 <View style={styles.section}>
-                    <Text>Diplom pro ...{obj['Jméno a příjmení']}</Text>
-                    <Text>Za {obj['Umístění']}. místo</Text>
-                    <Text>V kategorii {obj['Kategorie']}</Text>
-                    <Text>V disciplíně {obj['Disciplína']}</Text>
+                    <Text style={styles.heading}>{obj['Jméno a příjmení']}</Text>
+                    <Text style={styles.row}>získává diplom za {obj['Umístění']}. místo</Text>
+                    {/*<Text style={styles.row}>v kategorii {obj['Kategorie']}</Text>*/}
+                    <Text style={styles.row}>v disciplíně {obj['Disciplína']}</Text>
+                    <Text style={styles.footer}>Klepákův mlýn 2023</Text>
                 </View>
-                </Page>
-            ))}
+            </Page>
+        ))}
 
     </Document>
 );
