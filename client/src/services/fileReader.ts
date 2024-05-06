9af70c0bc449fa3ee84a28cd26cfd6b24ed5e12b
Pavel <pavelkurek@seznam.cz>
2024-05-06 18:36:31 +0200

    CLEANING UP

--- client/src/services/fileReader.ts
+++ client/src/services/fileReader.ts
@@ -1,51 +1,50 @@
-import * as XLSX from 'xlsx';
-import createDocument from './pdfCreator';
+import * as XLSX from "xlsx";
+import createDocument from "./pdfCreator";
 
 function fileReader(f: File | null): Promise<{ [key: string]: string }[]> {
-    return new Promise((resolve, reject) => {
-        if (!f) {
-            reject(new Error('File is null.'));
-            return;
-        }
+  return new Promise((resolve, reject) => {
+    if (!f) {
+      reject(new Error("File is null."));
+      return;
+    }
 
-        const reader = new FileReader();
-        reader.onload = async (evt) => {
-            const bstr = evt.target?.result as string;
-            const wb = XLSX.read(bstr, {type: 'binary'});
+    const reader = new FileReader();
+    reader.onload = async (evt) => {
+      const bstr = evt.target?.result as string;
+      const wb = XLSX.read(bstr, { type: "binary" });
 
-            const wsname = wb.SheetNames[0];
-            const ws = wb.Sheets[wsname];
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
 
-            const data = XLSX.utils.sheet_to_json(ws, {header: 1});
-            const [headerRow, ...rows] = data;
+      const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
+      const [headerRow, ...rows] = data;
 
-            const objects = rows.map((row) => {
-                const obj: { [key: string]: string } = {};
-                headerRow.forEach((header, index) => {
-                    obj[header] = row[index] as string;
-                });
-                return obj;
-            });
+      const objects = rows.map((row) => {
+        const obj: { [key: string]: string } = {};
+        headerRow.forEach((header, index) => {
+          obj[header] = row[index] as string;
+        });
+        return obj;
+      });
 
-            resolve(objects);
-        };
+      resolve(objects);
+    };
 
-        reader.onerror = (evt) => {
-            reject(new Error('Error reading file.'));
-        };
+    reader.onerror = (evt) => {
+      reject(new Error("Error reading file."));
+    };
 
-        reader.readAsBinaryString(f);
-    });
+    reader.readAsBinaryString(f);
+  });
 }
 
 async function processFile(f: File | null) {
-    try {
-        const objects = await fileReader(f);
-        return objects;
-
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const objects = await fileReader(f);
+    return objects;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export default processFile;
